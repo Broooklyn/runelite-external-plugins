397d3c5a0aeab419d8d7898916c4b73e07a8fadf
Broooklyn <54762282+Broooklyn@users.noreply.github.com>
2020-06-20 21:52:52 -0400

    Add notification messages for various potions' expiration

--- src/main/java/com/brooklyn/notificationmessages/NotificationMessagesPlugin.java
+++ src/main/java/com/brooklyn/notificationmessages/NotificationMessagesPlugin.java
@@ -18,11 +18,22 @@ import net.runelite.client.config.RuneLiteConfig;
 )
 public class NotificationMessagesPlugin extends Plugin
 {
+	// Personal Best
+	private static final String NEW_PB = "new personal best";
+	private static final String NEW_TOB_PB = "Personal best!";
+
+	// Pet Drops
 	private static final String FOLLOW_PET = "You have a funny feeling like you're being followed";
 	private static final String INVENTORY_PET = "You feel something weird sneaking into your backpack";
 	private static final String DUPE_PET = "You have a funny feeling like you would have been followed";
-	private static final String NEW_PB = "new personal best";
-	private static final String NEW_TOB_PB = "Personal best!";
+
+	// Potions
+	private static final String ANTIFIRE = "Your antifire potion has expired.";
+	private static final String SUPER_ANTIFIRE = "Your super antifire potion has expired.";
+	private static final String ANTIPOISON = "Your poison resistance has worn off.";
+	private static final String DIVINE_POTION = "The effects of the divine potion have worn off";
+	private static final String OVERLOAD = "The effects of overload have worn off, and you feel normal again.";
+	private static final String STAMINA = "Your stamina enhancement has expired.";
 
 	@Inject
 	private NotificationMessagesConfig config;
@@ -48,6 +59,13 @@ public class NotificationMessagesPlugin extends Plugin
 		switch (chatMessage.getType())
 			{
 			case GAMEMESSAGE:
+				if (chatMessage.getMessage().contains(NEW_PB) || chatMessage.getMessage().contains(NEW_TOB_PB))
+				{
+					if (config.notifyOnPersonalBest())
+					{
+						notifier.notify(config.personalBestMessage());
+					}
+				}
 				if (chatMessage.getMessage().contains(FOLLOW_PET))
 				{
 					if (config.notifyOnPet())
@@ -69,11 +87,39 @@ public class NotificationMessagesPlugin extends Plugin
 						notifier.notify(config.PetDupeMessage());
 					}
 				}
-				if (chatMessage.getMessage().contains(NEW_PB) || chatMessage.getMessage().contains(NEW_TOB_PB))
+				if (chatMessage.getMessage().contains(ANTIFIRE) || chatMessage.getMessage().contains(SUPER_ANTIFIRE))
 				{
-					if (config.notifyOnPersonalBest())
+					if (config.antifireNotification())
 					{
-						notifier.notify(config.personalBestMessage());
+						notifier.notify(config.antifireMessage());
+					}
+				}
+				if (chatMessage.getMessage().contains(ANTIPOISON))
+				{
+					if (config.antipoisonNotification())
+					{
+						notifier.notify(config.antipoisonMessage());
+					}
+				}
+				if (chatMessage.getMessage().contains(DIVINE_POTION))
+				{
+					if (config.divinePotionNotification())
+					{
+						notifier.notify(config.divinePotionMessage());
+					}
+				}
+				if (chatMessage.getMessage().contains(OVERLOAD))
+				{
+					if (config.overloadNotification())
+					{
+						notifier.notify(config.overloadMessage());
+					}
+				}
+				if (chatMessage.getMessage().contains(STAMINA))
+				{
+					if (config.staminaNotification())
+					{
+						notifier.notify(config.staminaMessage());
 					}
 				}
 					break;
@@ -86,11 +132,11 @@ public class NotificationMessagesPlugin extends Plugin
 					}
 				}
 				break;
-			case FRIENDSCHAT:
+			/*case FRIENDSCHAT:
 				if (chatMessage.getMessage().contains("Test"))
 				{
 					notifier.notify("ALERT: THIS IS A TEST");
-				}
+				}*/
 			}
 	}
 }
