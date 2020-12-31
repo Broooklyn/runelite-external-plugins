d56ca46289d9a3fcbfcf1893a44a2de54a95ac7a
Broooklyn <54762282+Broooklyn@users.noreply.github.com>
2020-12-31 00:04:44 -0500

    Update to comply with RL v1.6.36

--- src/main/java/com/brooklyn/notificationmessages/NotificationMessagesNotifier.java
+++ src/main/java/com/brooklyn/notificationmessages/NotificationMessagesNotifier.java
@@ -47,7 +47,7 @@ import net.runelite.client.eventbus.EventBus;
 import net.runelite.client.events.NotificationFired;
 import net.runelite.client.ui.ClientUI;
 import net.runelite.client.util.OSType;
-
+import javax.inject.Named;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -94,8 +94,6 @@ public class NotificationMessagesNotifier
         .addEscape('"', "'")
         .build();
 
-    private static final String appName = RuneLiteProperties.getTitle();
-
     private static final File NOTIFICATION_FILE = new File(RuneLite.RUNELITE_DIR, "notification.wav");
     private static final long CLIP_MTIME_UNLOADED = -2;
     private static final long CLIP_MTIME_BUILTIN = -1;
@@ -106,6 +104,7 @@ public class NotificationMessagesNotifier
     private final ScheduledExecutorService executorService;
     private final ChatMessageManager chatMessageManager;
     private final EventBus eventBus;
+    private final String appName;
     private final Path notifyIconPath;
     private final boolean terminalNotifierAvailable;
     private long lastClipMTime = CLIP_MTIME_UNLOADED;
@@ -118,7 +117,9 @@ public class NotificationMessagesNotifier
         final RuneLiteConfig runeliteConfig,
         final ScheduledExecutorService executorService,
         final ChatMessageManager chatMessageManager,
-        final EventBus eventBus)
+        final EventBus eventBus,
+        @Named("runelite.title") final String appName
+    )
         {
             this.client = client;
             this.clientUI = clientUI;
@@ -126,6 +127,7 @@ public class NotificationMessagesNotifier
             this.executorService = executorService;
             this.chatMessageManager = chatMessageManager;
             this.eventBus = eventBus;
+            this.appName = appName;
             this.notifyIconPath = RuneLite.RUNELITE_DIR.toPath().resolve("icon.png");
 
             // First check if we are running in launcher
